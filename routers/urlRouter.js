6125e2f06ad2e4b9acc879d24be66f5e9e151c64
Fernando Gomez <fergomez0801@gmail.com>
2026-03-03 19:24:59 -0600

    Add: editar direccion URL

--- routers/urlRouter.js
+++ routers/urlRouter.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { agregar } = require("../controllers/urlController")
+const { agregar, editar } = require("../controllers/urlController")
 
 
 router.post("/crear", agregar)
+router.put("/editar/:codigo", editar)
 
 module.exports = router
