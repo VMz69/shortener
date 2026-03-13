d7a409683187e802e0774a6700ec7397bd9ced77
Victor Emmanuel Velasco Martinez <viemvema@gmail.com>
2026-03-12 22:23:51 -0600

    Add delete endpoint for URL by ID William

--- routers/urlRouter.js
+++ routers/urlRouter.js
@@ -5,5 +5,10 @@ const { agregar, editar } = require("../controllers/urlController")
 
 router.post("/crear", agregar)
 router.put("/editar/:codigo", editar)
+// para Endpoint para crear una URL corta 
+router.post("/crear", agregar);
+
+// y paraEndpoint para borrar por ID
+router.delete("/:id", borrarUrlPorId);
 
 module.exports = router
